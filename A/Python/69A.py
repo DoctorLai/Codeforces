032e7706c256095f5c77388eea711b689d9a1ffa
Dr Zhihua Lai <dr@zhihua-lai.com>
2012-05-26 20:39:01 +0100

    69A Young Physicist

---
+++ A/Python/69A.py
@@ -0,0 +1,15 @@
+
+n = int(raw_input())
+
+sx = 0
+sy = 0
+sz = 0
+
+for i in range(0, n):
+    ss = raw_input().split(" ")
+    sx += int(ss[0])
+    sy += int(ss[1])
+    sz += int(ss[2])
+
+print "YES" if sx == 0 and sy == 0 and sz == 0 else "NO"
+    
